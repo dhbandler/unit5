0d48e769eab8b38b1f386ba9ee7ff03bb0ebf983
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-04 08:26:58 -0400

    Updated from Brython Server: 5/4/2018 8:26:56 AM

--- mySort.py
+++ mySort.py
@@ -9,7 +9,15 @@ from time import time
 N = 10 #how many numbers will be sorted
 
 def mySort(A):
-
+    i=1
+    while i < len(A):
+        x = A[i]
+        j = i - 1
+        while j>= 0 and j[1].x:
+            A[j+1] = A[j]
+            j = j-1
+        A[j+1] = x
+        i = i+1
     return A
     
 if __name__ == '__main__':
